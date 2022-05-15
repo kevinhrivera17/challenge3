f2e30b293a994a2663c94fa97a58f73d4d202956
Kevin Rivera <khernandez@applaudostudios.dev>
2022-05-14 18:40:29 -0600

    Updating content on the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,8 +57,8 @@ const descriptionStyle = {
 }
 
 const docLink = {
-  text: "Documentación por Gatsby de CI",
-  url: "https://www.gatsbyjs.com/docs/",
+  text: "Documentation shared by the Team on ONE NOTE",
+  url: "https://applaudostudios.sharepoint.com/:o:/r/sites/TAWSTraineeProgram-Trainers/Shared%20Documents/Trainers/AWS%20Content?d=w50a0d25d0f224143be20db3546dd9389&csf=1&web=1&e=aKaZ3v",
   color: "#8954A8",
 }
 
@@ -81,46 +81,48 @@ const badgeStyle = {
 // data
 const links = [
   {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
+    text: "Week 1 - DevOps Culture ",
+    url: "#",
     description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
+      "This week is all about introducing you to the DevOps mindset. DevOps is not a role or a set of tools, it's a way of working and culture of collaboration. ",
     color: "#E95800",
   },
   {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
+    text: "Week 2 - Linux & Bash scripting",
+    url: "#",
     description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
+      "GNU/Linux powers most of our modern world, it can be found from the smallest IoT devices to the most powerful supercomputers and cloud vendors. A good understanding of how to use Linux and its tools is very important for a DevOps engineer, since most of the infrastructure that we manage runs some flavor of Linux. ",
     color: "#1099A8",
   },
   {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
+    text: "Week 3 - Git & CI/CD",
+    url: "#",
     description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
+      "Git is the most popular software for source control versioning. It enables teams to collaborate and work simultaneously on the same code, and it's multiple concepts such as branches, merging and tags, enable advanced workflows that give place to Continuous Integration and Continuous Delivery. ",
     color: "#BC027F",
   },
   {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
+    text: "Week 4 - Content Blocked",
+    url: "#",
+    badge: true,
     description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
+      "Content Blocked.",
     color: "#0D96F2",
   },
   {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
+    text: "Week 5 - Content Blocked",
+    url: "#",
+    badge: true,
     description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
+      "Content Blocked.",
     color: "#8EB814",
   },
   {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
+    text: "Week 6 - Content Blocked",
+    url: "#",
     badge: true,
     description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
+      "Content Blocked.",
     color: "#663399",
   },
 ]
@@ -129,21 +131,14 @@ const links = [
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
-      <title>Home Page</title>
+      <title>Challenge | Week 3</title>
       <h1 style={headingStyles}>
-        Congratulations
+        AWS Trainee
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
+        <span style={headingAccentStyles}>Notes related to the Trainee </span>
       </h1>
       <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
+        <code style={codeStyles}>Kevin Orlando Hernandez Rivera | Week 3</code> 
       </p>
       <ul style={listStyles}>
         <li style={docLinkStyle}>
@@ -173,10 +168,6 @@ const IndexPage = () => {
           </li>
         ))}
       </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
     </main>
   )
 }
